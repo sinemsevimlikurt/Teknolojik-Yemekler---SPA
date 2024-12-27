164fefb2793f203541c8a76d18c15116d885130f
sinemsevimlikurt <sevimlikurtsinem@gmail.com>
2024-12-28 00:05:32 +0300

    homepage done

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../css/HomePage.css"
+
+function HomePage() {
+  return (
+    <div className="home">
+        <h1>Teknolojik Yemekler</h1>
+    <div className="home-text">
+        <p>KOD ACIKTIRIR
+        PİZZA, DOYURUR</p>
+    </div>
+        <button className="home-button">ACIKTIM</button>
+    </div>
+  )
+}
+
+export default HomePage
